1601fd5e873367dfef3298590973ca42930de1e2
Isaac-Carrascal <isaac.carrascal1@gmail.com>
2020-08-27 17:04:45 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+basedir=os.dir.abspath(os.path.dirname(__file__))
+class Config(object):
+    SECRET_KEY=os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI=os.path.get("DATABASE_URL") or \
+        'sqllite:///' +os.path.join(basedir,'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS= False
